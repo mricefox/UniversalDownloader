43cb59834659afabdc3bba68d8eb6fa2c39c0e9a
MrIceFox <extremetsa@gmaiil.com>
2015-11-23 18:10:13 +0800

    test

--- app/src/main/java/com/mf/bourne/mfdownloader/MainActivity.java
+++ app/src/main/java/com/mf/bourne/mfdownloader/MainActivity.java
@@ -24,6 +24,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
     }
 
 
